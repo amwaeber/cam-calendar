a4b1e3af9f9a0c9fde57fc79e24d0aeb0e611623
Andreas Waeber <amwaeber@gmail.com>
2025-07-24 06:28:31 +0100

    switched to managed postgresql database

--- cam_calendar/settings/base.py
+++ cam_calendar/settings/base.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from urllib.parse import urlparse, parse_qsl
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -52,10 +53,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cam_calendar.wsgi.application'
 
+
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
